e937dd69839339c505bf6487d910d1084204211a
Mark Nodson <mark.nodson@gmail.com>
2024-04-06 17:02:52 +0100

    add pitch deck option to ideapros page

--- blackwoodops-website/src/app/idea-pros/idea-pros.component.ts
+++ blackwoodops-website/src/app/idea-pros/idea-pros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-idea-pros',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class IdeaProsComponent {
 
+  public componentStates = new Map<string, boolean>();
+
+  toggleState(section: string) {
+
+    this.resetAllOtherStates(section);
+
+
+
+    if (this.componentStates.has(section)) {
+      const currentValue = this.componentStates.get(section);
+      this.componentStates.set(section, !currentValue);
+    } else {
+      this.componentStates.set(section, false);
+    }
+  }
+
+  resetAllOtherStates(section: string) {
+    this.componentStates.forEach((value, key) => {
+      if (key !== section) {
+        this.componentStates.set(key, true);
+      }
+    });
+  }
+
+  getState(section: string) {
+    if (this.componentStates.has(section)) {
+      return !!this.componentStates.get(section);
+    } else {
+      return true;
+    }
+  }
 }
